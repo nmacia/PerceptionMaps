d2442c5bd62aa7e49934a7409c27e5284b6b7a43
Núria Macià <macia.nuria@gmail.com>
2018-04-03 17:42:07 -0400

    Refactor input data
    Load data from csv file

--- map/js/map.js
+++ map/js/map.js
@@ -7,7 +7,6 @@ var blackAndWhiteLayer = L.tileLayer.provider('OpenStreetMap.BlackAndWhite');
 var stamenTonerLayer = L.tileLayer.provider('Stamen.Toner');
 var WImagEsriLayer = L.tileLayer.provider('Esri.WorldImagery');
 
-
 mymap.setView([51.43555, 5.48055], 17); 
 mymap.addLayer(stamenTonerLayer);
 
@@ -19,15 +18,15 @@ var baseMaps = {
 };
 
 var happyIcon = L.icon({
-  iconUrl: 'img/happy_f.png'
+  iconUrl: 'img/happy-f.png'
 });
 
 var sadIcon = L.icon({
-  iconUrl: 'img/sad_f.png'
+  iconUrl: 'img/sad-f.png'
 });
 
 var angryIcon = L.icon({
-  iconUrl: 'img/angry_f.png'
+  iconUrl: 'img/angry-f.png'
 });
 
 var categories = {},
@@ -37,33 +36,39 @@ var overlays = {},
 categoryName,
 categoryArray;
 
-var geojsonLayer = new L.GeoJSON.AJAX("test-eindhoven.geojson", {
-  pointToLayer: function (feature, latlng) {
-    switch (feature.properties.Emotion) {
-      case 'happy': return L.marker(latlng, {icon: happyIcon});
-      case 'sad': return L.marker(latlng, {icon: sadIcon});
-      case 'angry': return L.marker(latlng, {icon: angryIcon});
-    }
-  },
-  onEachFeature(feature, layer) {
-    var classNameInfoWindow = emotionCategoryInfoWindowStyle(feature);
-    var customOptions = {
-      'className' : classNameInfoWindow
-    };
-
-    layer.bindPopup(definePopup(feature),customOptions);
-
-    category = feature.properties.Emotion;
-
-    if (typeof categories[category] === "undefined") {
-      categories[category] = [];
-    }
-
-    categories[category].push(layer);
-  }
+jQuery.get('output.csv', function(csvContents) {
+  var geocsvLayer = L.geoCsv(csvContents, {
+    latitudeTitle: 'GPSLatitude',
+    longitudeTitle: 'GPSLongitude',
+    firstLineTitles: false, 
+    titles: ['SourceFile', 'GPSLatitude', 'GPSLongitude', 'Emotion'],
+    fieldSeparator: ',',
+    pointToLayer: function (feature, latlng) {
+      switch (feature.properties.emotion) {
+        case 'happy': return L.marker(latlng, {icon: happyIcon});
+        case 'sad': return L.marker(latlng, {icon: sadIcon});
+        case 'angry': return L.marker(latlng, {icon: angryIcon});
+      }
+    },
+    onEachFeature(feature, layer) {
+      var classNameInfoWindow = feature.properties.emotion + "-bg";
+      var customOptions = {
+        'className' : classNameInfoWindow
+      };
+      layer.bindPopup(definePopup(feature),customOptions);
+      category = feature.properties.emotion;
+      if (typeof categories[category] === "undefined") {
+        categories[category] = [];
+      }
+      categories[category].push(layer);
+    }   
+  });
+  
+  mymap.addLayer(geocsvLayer);
 });
 
-geojsonLayer.on('data:loaded', function() {
+// TODO: Remove overlays if not necessary
+/*geojsonLayer.on('data:loaded', function() {
   for (categoryName in categories) {
     categoryArray = categories[categoryName];
     overlays[categoryName] = L.layerGroup(categoryArray);
@@ -75,34 +80,34 @@ geojsonLayer.on('data:loaded', function() {
 
   L.control.layers(baseMaps, overlays).addTo(mymap);
 });
+*/
 
- function definePopup(feature) {
-   var props = feature.properties;
-   var emotion = props.Emotion;
-   var popupText = `
-    <div class="containerinfo">
-      <div class= "box-logoandemotion">
-        <div class= "logo sqre">
-          <img src="img/logodm4ym.png">
-        </div>
-        <div class= "emotion sqre">` +
-'         <img src="svg/'+ emotion+ '.svg" /> ' +
-'         <p class="emotion-name">'+emotion +'</p> '+
-`       </div>
+function definePopup(feature) {
+   
+  var emotion = feature.properties.emotion;
+  var sourceFile = feature.properties.sourcefile;
+  
+  var popupText = `
+  <div class="containerinfo">
+    <div class= "box-logoandemotion">
+      <div class= "logo sqre">
+        <img src="img/logodm4ym.png">
       </div>
-      <div class= "box-text">
-        <!--get from table description --> ` +
-'        <p><b>date:</b> ' + props.Date + ' ' + props.Time + '</p> ' +
-'        <p><b>description:</b>'+ ' ' + props.Notes +'</p> ' +
-`     </div>
-      <div class= "box-img"> ` +
-        "<img src='"+ props.SourceFile +"' />" +             
-`     </div>      
+      <div class= "emotion sqre">` +
+  '         <img src="svg/'+ emotion + '.svg" /> ' +
+  '         <p class="emotion-name">'+ emotion + '</p> '+
+  `       </div>
     </div>
-`;
-   return popupText;
-}
+    <div class= "box-text">
+      <!--get from table description --> ` +
+  /*'        <!p><b>date:</b> ' + props.Date + ' ' + props.Time + '</p> ' +*/
+  /*'        <p><b>description:</b>'+ ' ' + props.Notes +'</p> ' +*/
+  `     </div>
+    <div class= "box-img"> ` +
+      "<img src='"+ sourceFile +"' />" +             
+  `     </div>      
+  </div>
+  `;
 
-function emotionCategoryInfoWindowStyle(feature) {
-  return feature.properties.Emotion + "-bg";
+  return popupText;
 }
